b400a7edf6bb13a5eafe93c356a5ceb76eb50168
Krishna sai Patel <patel@Krishnas-MacBook-Air.local>
2020-08-15 20:55:29 +0530

    build

---
+++ build/precache-manifest.a565948fff22817f8ebcf2a1a854120a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f5c1821ed955a5ee5290b6aa5712f2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1833ad69599deeaf216",
+    "url": "/static/css/2.465d1ef4.chunk.css"
+  },
+  {
+    "revision": "2933def14d916addd19b",
+    "url": "/static/css/main.81e94b80.chunk.css"
+  },
+  {
+    "revision": "b1833ad69599deeaf216",
+    "url": "/static/js/2.5285eeb1.chunk.js"
+  },
+  {
+    "revision": "042f2b0c18596f1cdf6828b6d6a9f155",
+    "url": "/static/js/2.5285eeb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2933def14d916addd19b",
+    "url": "/static/js/main.d1fd0795.chunk.js"
+  },
+  {
+    "revision": "2cb9eb50baaf7e13b0f5",
+    "url": "/static/js/runtime-main.16f44215.js"
+  }
+]);
